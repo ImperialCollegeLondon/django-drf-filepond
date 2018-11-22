e7d0148caa906cbc6863ba6b3f3485c07cb546f4
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2018-11-22 20:18:58 +0000

    Resolved issues with invalid storage location test, added further process tests.

--- tests/test_process_view.py
+++ tests/test_process_view.py
@@ -5,11 +5,10 @@ from io import BytesIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 import os
 from django.urls import reverse
-from django.test.client import encode_multipart
+from django.test.client import encode_multipart, RequestFactory
 import uuid
-from unittest.mock import patch
 from django.core.files.storage import FileSystemStorage
-import django_drf_filepond.models as models
+import django_drf_filepond.views as views
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig()
@@ -38,13 +37,37 @@ class ProcessTestCase(TestCase):
                          'Response data is not of the correct length.')
     
     def test_process_invalid_storage_location(self):
-        models.storage = FileSystemStorage(location='/django_test')
+        old_storage = views.storage
+        views.storage = FileSystemStorage(location='/django_test')
         (encoded_form, content_type) = self._get_encoded_form('testfile.dat')
         
-        response = self.client.post(reverse('django_drf_filepond_process'),
-                                data=encoded_form, content_type=content_type)
+        rf = RequestFactory()
+        req = rf.post(reverse('django_drf_filepond_process'),
+                      data=encoded_form, content_type=content_type)
+        pv = views.ProcessView.as_view()
+        response = pv(req)
+        views.storage = old_storage
         self.assertEqual(response.status_code, 500, 'Expecting 500 error due'
                          ' to invalid storage location.')
+        self.assertEqual(response.data, ('The file upload path settings '
+                        'are not configured correctly.'), ('Expecting error '
+                        'showing path settings are configured incorrectly.'))
+    
+    def test_process_invalid_data(self):
+        upload_form = {'somekey': SimpleUploadedFile('test.txt', 
+                                                      self.test_data.read())}
+        enc_form = encode_multipart('abc', upload_form)
+        rf = RequestFactory()
+        req = rf.post(reverse('django_drf_filepond_process'), data=enc_form,
+                      content_type='multipart/form-data; boundary=abc')
+        pv = views.ProcessView.as_view()
+        response = pv(req)
+        self.assertEqual(response.status_code, 400, 'Expecting 400 error due'
+                         ' to invalid data being provided.')
+        self.assertTrue('detail' in response.data, 
+                      'Error detail missing in response.')
+        self.assertIn(response.data['detail'], ('Invalid request data has '
+                    'been provided.'))
         
     def _get_encoded_form(self, filename):
         f = SimpleUploadedFile(filename, self.test_data.read())
