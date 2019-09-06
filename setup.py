196d29fc295779fcbcee8476c52082575c4348c9
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2019-09-06 18:11:26 +0100

    minor version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="django-drf-filepond",
-    version="0.1.1",
+    version="0.1.2",
     description="Filepond server app for Django REST Framework",
     long_description=long_description,
     long_description_content_type='text/markdown',
