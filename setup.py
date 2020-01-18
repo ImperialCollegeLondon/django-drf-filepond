fab6bc8415c44c329c9b3236157cf30281b911f9
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2020-01-18 19:19:12 +0000

    Updates to support Django 3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="django-drf-filepond",
-    version="0.3.0",
+    version="0.4.0",
     description="Filepond server app for Django REST Framework",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -22,12 +22,14 @@ setup(
     include_package_data=True,
     install_requires=[
         "Django<2.0.0;python_version=='2.7'",
-        "Django<3.0.0;python_version>='3.5'",
+        "Django<3.0.0;python_version=='3.5'",
+        "Django>=3.0.2;python_version>='3.6'",
         "djangorestframework==3.9.3;python_version=='2.7'",
         "djangorestframework>=3.9.3;python_version>='3.5'",
         "shortuuid>=0.5.0",
         "requests>=2.20.1",
-        "django-storages>=1.7.1"
+        "django-storages>=1.8",
+        "six>=1.14.0"
     ],
     tests_require=[
         "nose",
