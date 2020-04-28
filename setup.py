fc3a36c608017d1a12a435612c5d20276415f207
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2020-04-28 16:18:29 +0100

    updated version for 0.3.1 maintenance release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="django-drf-filepond",
-    version="0.3.0",
+    version="0.3.1",
     description="Filepond server app for Django REST Framework",
     long_description=long_description,
     long_description_content_type='text/markdown',
