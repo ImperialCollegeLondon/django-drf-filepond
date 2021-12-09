b9a5e46d53580a3295df8a8ebe93f4890e000510
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2021-12-09 19:20:20 +0000

    Django4 updates retaining py 2.7 support.

--- django_drf_filepond/urls.py
+++ django_drf_filepond/urls.py
@@ -4,16 +4,38 @@ Based on the server-side configuration details
 provided at:
 https://pqina.nl/filepond/docs/patterns/api/server/#configuration
 """
-from django.conf.urls import url
+import six
+if six.PY2:
+    from django.conf.urls import url
+else:
+    from django.urls import re_path
 from django_drf_filepond.views import ProcessView, RevertView, LoadView,\
      RestoreView, FetchView, PatchView
 
-urlpatterns = [
-    url(r'^process/$', ProcessView.as_view(), name='process'),
-    url(r'^patch/(?P<chunk_id>[0-9a-zA-Z]{22})$', PatchView.as_view(),
-        name='patch'),
-    url(r'^revert/$', RevertView.as_view(), name='revert'),
-    url(r'^load/$', LoadView.as_view(), name='load'),
-    url(r'^restore/$', RestoreView.as_view(), name='restore'),
-    url(r'^fetch/$', FetchView.as_view(), name='fetch')
-]
+#############################################################################
+# PYTHON 2 SUPPORT
+# Retaining Python 2 compatibility for now. However, with the switch to
+# Django 4.0, django.conf.urls.url has been removed and we need to switch
+# to paths - this Python 2 specific block will ultimately be removed when
+# Python 2.7 support is dropped along with support for django 1.x.
+#############################################################################
+if six.PY2:
+    urlpatterns = [
+        url(r'^process/$', ProcessView.as_view(), name='process'),
+        url(r'^patch/(?P<chunk_id>[0-9a-zA-Z]{22})$', PatchView.as_view(),
+            name='patch'),
+        url(r'^revert/$', RevertView.as_view(), name='revert'),
+        url(r'^load/$', LoadView.as_view(), name='load'),
+        url(r'^restore/$', RestoreView.as_view(), name='restore'),
+        url(r'^fetch/$', FetchView.as_view(), name='fetch')
+    ]
+else:
+    urlpatterns = [
+        re_path(r'^process/$', ProcessView.as_view(), name='process'),
+        re_path(r'^patch/(?P<chunk_id>[0-9a-zA-Z]{22})$', PatchView.as_view(),
+                name='patch'),
+        re_path(r'^revert/$', RevertView.as_view(), name='revert'),
+        re_path(r'^load/$', LoadView.as_view(), name='load'),
+        re_path(r'^restore/$', RestoreView.as_view(), name='restore'),
+        re_path(r'^fetch/$', FetchView.as_view(), name='fetch')
+    ]
