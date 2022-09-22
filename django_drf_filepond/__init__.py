911647ae6d3497977dd81ae6eb6a19e6ea8eaa49
Jeremy Cohen <jcohen02@users.noreply.github.com>
2022-09-22 19:53:09 +0100

    Completed switch from nose to pytest and tox with multi-env testing.

--- django_drf_filepond/__init__.py
+++ django_drf_filepond/__init__.py
@@ -1,1 +1,4 @@
+# This setting is remaining in place to support older versions of Python/Django
+# however it is deprecated and removed in Django 4.1 where the setting
+# is detected automatically.
 default_app_config = 'django_drf_filepond.apps.DjangoDrfFilepondConfig'
