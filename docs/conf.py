102f15e2c89f29dabd5c3beeabcccf35c52aeaf9
Jeremy Cohen <jcohen02@users.noreply.github.com>
2023-12-14 13:06:56 +0000

    Update version string and docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'django-drf-filepond'
-copyright = '2019, Jeremy Cohen, Imperial College London'
-author = 'Jeremy Cohen'
+copyright = '2019-2023, Jeremy Cohen, Imperial College London and contributors'
+author = 'Jeremy Cohen and contributors'
 
 # The short X.Y version
 version = ''
@@ -59,7 +59,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
