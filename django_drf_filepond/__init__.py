39e573356b16fd892eb89bde473bc363356646bc
Jeremy Cohen <jcohen02@users.noreply.github.com>
2025-01-02 20:29:56 +0000

    Adding py27/py35 support deprecation warning. Added py312 to tox config.

--- django_drf_filepond/__init__.py
+++ django_drf_filepond/__init__.py
@@ -1,4 +1,20 @@
+import sys
+import warnings
+
+from django_drf_filepond.deprecation import DjangoDrfFilepondDeprecationWarning
+
 # This setting is remaining in place to support older versions of Python/Django
 # however it is deprecated and removed in Django 4.1 where the setting
 # is detected automatically.
 default_app_config = 'django_drf_filepond.apps.DjangoDrfFilepondConfig'
+
+# Trigger display of deprecation warning if running with py27 or py35
+if ((sys.version_info[:2] == (2, 7)) or
+    (sys.version_info[:2] == (3, 5))):
+    warnings.warn(
+        "Python versions 2.7 and 3.5 will no longer be supported in "
+        "future releases of django-drf-filepond. 0.5.x releases are "
+        "the last to provide support for Python 2.7 and 3.5.",
+        DjangoDrfFilepondDeprecationWarning,
+        stacklevel=2,
+    )
---
+++ django_drf_filepond/deprecation.py
@@ -0,0 +1,4 @@
+# This deprecation warning is used to warn developers when a django-drf-filepond
+# feature or support for a particular Python version is deprecated.
+class DjangoDrfFilepondDeprecationWarning(UserWarning):
+    pass
